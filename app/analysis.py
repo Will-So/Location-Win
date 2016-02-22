2948d9942addacfdc7b4c6b6b5804e427372daec
Will Sorenson <ryan.d.sorenson@gmail.com>
2016-02-21 20:55:37 -0500

    add analysis

---
+++ app/analysis.py
@@ -0,0 +1,5 @@
+"""
+Web App that shows various analyses of the data. Targeted only towards people with a technical background
+"""
+
+from flask import Flask, render_template
---
+++ scripts/key_words.py
@@ -0,0 +1,4 @@
+"""
+Finds words that are indicative of low success and high success.
+
+"""
